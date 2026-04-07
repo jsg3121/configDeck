98c11335c1b9cac60888d8efd02b26be689e57a5
jsg3121 <xodm95@gmail.com>
2026-04-07 22:17:38 +0900

    🛠️ fix : 공통 레이아웃 생성

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,16 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://configdeck.dev',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ko'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+
   integrations: [svelte()],
 
   vite: {
